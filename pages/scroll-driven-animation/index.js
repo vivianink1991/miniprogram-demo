e65b34e2c21a031d44901533f2f0ac74867abb24
jiaxin.j.li <jiaxin.j.li@daimler.com>
2021-01-03 16:50:54 +0800

    update scroll driven animation

--- pages/scroll-driven-animation/index.js
+++ pages/scroll-driven-animation/index.js
@@ -5,13 +5,15 @@ Page({
 
   data: {
     contents: {},
-    navTotalHeight: 0
+    navTotalHeight: 0,
+    contentHeight: 0
   },
 
   onLoad: function (options) {
     const { navTotalHeight, screen } = App.globalData
     this.setData({
       navTotalHeight,
+      contentHeight: screen.screenHeight - navTotalHeight,
       contents: scrollMaterials
     })
   },
@@ -20,86 +22,192 @@ Page({
     this.scrollAnimate()
   },
 
+  scrollHandler(e) {
+    console.log('e', e.detail.scrollTop)
+  },
   scrollAnimate() {
-    const { screen } = App.globalData
     this.createSelectorQuery().select('#scroller').fields({
       scrollOffset: true,
       size: true
     }, res => {
-      console.log('res', res)
-      // section_1 title
-      this.animate('.section_1 .title', [
-        { 
-          opacity: 0.8,
-          transform: 'translateY(-10px)',
-          offset: 0
-        },
-        { 
-          opacity: 0.2,
-          transform: 'translateY(-20px)',
-          offset: 0.5
-        },
-        { 
-          opacity: 0,
-          transform: 'translateY(-30px)',
-          offset: 1
-        },
-      ], 1000, {
-        scrollSource: '#scroller',
-        timeRange: 1000,
-        startScrollOffset: 10,
-        endScrollOffset: 80
-      })
-  
-      // section_1 img
-      this.animate('.section_1 .img', [
-        { 
-          opacity: 0.8,
-          transform: 'translateY(0%)',
-          offset: 0
-        },
-        { 
-          opacity: 0.2,
-          transform: 'translateY(-30%)',
-          offset: 0.5
-        },
-        { 
-          opacity: 0,
-          transform: 'translateY(-50%)',
-          offset: 1
-        },
-      ], 2000, {
-        scrollSource: '#scroller',
-        timeRange: 2000,
-        startScrollOffset: 10,
-        endScrollOffset: screen.screenHeight * 0.8
-      })
-
-      // section_1 desc
-      this.animate('.section_1 .desc', [
-        { 
-          opacity: 0.8,
-          transform: 'translateY(0%)',
-          offset: 0
-        },
-        { 
-          opacity: 0.2,
-          transform: 'translateY(-300%)',
-          offset: 0.5
-        },
-        { 
-          opacity: 0,
-          transform: 'translateY(-600%)',
-          offset: 1
-        },
-      ], 2000, {
-        scrollSource: '#scroller',
-        timeRange: 2000,
-        startScrollOffset: 10,
-        endScrollOffset: screen.screenHeight * 0.8
-      })
-
+      this.animateSectionOne(res)
+      this.animateSectionTwo(res)
     }).exec()
+  },
+
+  animateSectionOne(scrollInfo) {
+    const { contentHeight } = this.data
+    // section_1 title
+    this.animate('.section_1 .title', [
+      { 
+        opacity: 0.8,
+        transform: 'translateY(-10px)',
+        offset: 0
+      },
+      { 
+        opacity: 0.2,
+        transform: 'translateY(-20px)',
+        offset: 0.5
+      },
+      { 
+        opacity: 0,
+        transform: 'translateY(-30px)',
+        offset: 1
+      },
+    ], 1000, {
+      scrollSource: '#scroller',
+      timeRange: 1000,
+      startScrollOffset: 10,
+      endScrollOffset: 80
+    })
+
+    // section_1 img
+    this.animate('.section_1 .img', [
+      { 
+        opacity: 0.8,
+        transform: 'translateY(0%)',
+        offset: 0
+      },
+      { 
+        opacity: 0.2,
+        transform: 'translateY(-30%)',
+        offset: 0.5
+      },
+      { 
+        opacity: 0,
+        transform: 'translateY(-50%)',
+        offset: 1
+      },
+    ], 2000, {
+      scrollSource: '#scroller',
+      timeRange: 2000,
+      startScrollOffset: 10,
+      endScrollOffset: contentHeight * 0.8
+    })
+
+    // section_1 desc
+    this.animate('.section_1 .desc', [
+      { 
+        opacity: 0.8,
+        transform: 'translateY(0%)',
+        offset: 0
+      },
+      { 
+        opacity: 0.2,
+        transform: 'translateY(-200%)',
+        offset: 0.5
+      },
+      { 
+        opacity: 0,
+        transform: 'translateY(-400%)',
+        offset: 1
+      },
+    ], 2000, {
+      scrollSource: '#scroller',
+      timeRange: 2000,
+      startScrollOffset: 10,
+      endScrollOffset: contentHeight * 0.5
+    })
+  },
+
+  animateSectionTwo(scrollInfo) {
+    console.log('scrollInfo', scrollInfo)
+    const { contentHeight } = this.data
+    // section_2 title
+    this.animate('.section_2 .title', [
+      { 
+        opacity: 0,
+        transform: 'translateY(-10px) scale(0.8)',
+        offset: 0
+      },
+      { 
+        opacity: 0.5,
+        transform: 'translateY(-30px) scale(1.1)',
+        offset: 0.5
+      },
+      { 
+        opacity: 1,
+        transform: 'translateY(-50px) scale(1.2)',
+        offset: 1
+      }
+    ], 2000, {
+      scrollSource: '#scroller',
+      timeRange: 2000,
+      startScrollOffset: contentHeight * 0.5,
+      endScrollOffset: contentHeight * 0.9
+    })
+
+    // section_2 img
+    this.animate('.section_2 .img', [
+      { 
+        opacity: 0,
+        transform: 'translateY(-10px) scale(0.7)',
+        offset: 0
+      },
+      { 
+        opacity: 0.5,
+        transform: 'translateY(-30px) scale(0.8)',
+        offset: 0.5
+      },
+      { 
+        opacity: 1,
+        transform: 'translateY(-50px) scale(1)',
+        offset: 1
+      }
+    ], 2000, {
+      scrollSource: '#scroller',
+      timeRange: 2000,
+      startScrollOffset: contentHeight * 0.6,
+      endScrollOffset: contentHeight * 0.9
+    })
+
+    // section_2 desc
+    this.animate('.section_2 .desc', [
+      { 
+        opacity: 0,
+        transform: 'translateY(0px)',
+        offset: 0
+      },
+      { 
+        opacity: 0.5,
+        transform: 'translateY(-20px)',
+        offset: 0.5
+      },
+      { 
+        opacity: 1,
+        transform: 'translateY(-40px)',
+        offset: 1
+      }
+    ], 2000, {
+      scrollSource: '#scroller',
+      timeRange: 2000,
+      startScrollOffset: contentHeight * 0.8,
+      endScrollOffset: contentHeight * 1
+    })
+
+    // section_2
+    this.animate('.section_2', [
+      { 
+        opacity: 1,
+        // transform: 'translateY(-50px)',
+        offset: 0
+      },
+      { 
+        opacity: 0.5,
+        // transform: 'translateY(-80px) scale(1.2)',
+        offset: 0.5
+      },
+      { 
+        opacity: 0,
+        // transform: 'translateY(-100px) scale(1.2)',
+        offset: 1
+      }
+    ], 2000, {
+      scrollSource: '#scroller',
+      timeRange: 2000,
+      startScrollOffset: contentHeight * 1,
+      endScrollOffset: contentHeight * 1.5
+    })
   },
   onShow: function () {
 
